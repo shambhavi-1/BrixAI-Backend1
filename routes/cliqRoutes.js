2d2e7ca2b5d7cd2913e1adcafe5719dd423d8256
shambhavi-1 <150891658+shambhavi-1@users.noreply.github.com>
2025-11-30 19:48:51 +0530

    Delete routes directory

--- routes/cliqRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cliqController = require("../controllers/cliqController");
-
-// This is the single endpoint Cliq will call for all /bp commands
-router.post("/events", cliqController.handleCliqCommand);
-
-module.exports = router;
--- routes/webhookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-// Zoho Cliq webhook endpoints
-router.post('/channel-message', webhookController.handleChannelMessage);
-router.post('/bot-command', webhookController.handleBotCommand);
-
-module.exports = router;
