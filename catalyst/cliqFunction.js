eb5e9534732d80ba2b3263912b277bb9a33c73a2
shambhavi-1 <150891658+shambhavi-1@users.noreply.github.com>
2025-11-30 12:04:11 +0530

    Delete catalyst directory

--- catalyst/cliqFunction.js
+++
@@ -1,9 +0,0 @@
-const connectDB = require('../config/db');
-const cliqController = require('../controllers/cliqController');
-
-connectDB();
-
-module.exports = async function(req, res) {
-  req.body = req.body || req.payload || {};
-  return cliqController.handleCliqCommand(req, res);
-};
