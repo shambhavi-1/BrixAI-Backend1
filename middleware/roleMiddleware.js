feed7630cf22ef4434efabc7fdbc02f1948f7530
shambhavi-1 <150891658+shambhavi-1@users.noreply.github.com>
2025-11-30 19:47:56 +0530

    Delete middleware directory

--- middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-// backend/middleware/roleMiddleware.js
-module.exports = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-    next();
-  };
-};
